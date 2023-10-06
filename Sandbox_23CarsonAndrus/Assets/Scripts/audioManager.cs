056d71c1ef8a5ba1b43d215852e38c48dae52fad
CarsonAndrus <carson.andrus@lethbridgecollege.ca>
2023-10-06 12:41:29 -0600

    First  Commit (Fixed file path)

---
+++ Sandbox_23CarsonAndrus/Assets/Scripts/audioManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioManager : MonoBehaviour
+{
+    
+
+    public AudioSource soundPlayer;
+    
+    
+    
+ 
+    //plays audiosource
+    public void playSoundEffect()
+    {
+        soundPlayer.Play();
+    }
+    //stops audiosource
+    public void stopSoundEffect()
+    {
+        soundPlayer.Stop();
+    }
+
+
+
+
+}
